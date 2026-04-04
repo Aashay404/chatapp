39d2d37cc7dc5b4d765eaaafa9ab240ee4147c3a
Aashay <aashay.shelar.aids.2023@vpkbiet.org>
2026-04-04 09:35:10 +0530

    Update MessageBubble.jsx

--- chatapp/src/components/chat/MessageBubble.jsx
+++ chatapp/src/components/chat/MessageBubble.jsx
@@ -6,7 +6,7 @@ const MessageBubble = ({ text, isOwn }) => {
       className={`max-w-sm px-4 py-3 rounded-2xl text-white ${
         isOwn
           ? "bg-blue-500/80 self-end"
-          : "bg-white/10 self-start"
+          : "bg-white/100 self-start"
       }`}
     >
       {text}
